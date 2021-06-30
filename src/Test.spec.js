e74bda7b0d71f41b79ba417fc826d696f91d1278
eunjae-lee <karis612@gmail.com>
2021-06-30 16:17:01 +0200

    .

--- src/Test.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from "@vue/test-utils";
-import Test from "./Test.vue";
-
-test("uses mounts", async () => {
-	const wrapper = mount(Test);
-	expect(wrapper.html()).toMatchInlineSnapshot(
-		`"<div><button title=\\"this is a button\\" class=\\"hello\\">click</button></div>"`
-	);
-});
